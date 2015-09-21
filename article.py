d6e2113e7dd57b4183565de975ac4fbdcd0ed128
Allen Guo <guoguo12@gmail.com>
2015-09-21 02:01:37 -0700

    Auto-update last-modified date

--- article.py
+++ article.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
-import sys
+import datetime, sys
 
 with open('index.html', 'r') as f:
     lines = f.readlines()
     try:
-        ln = lines.index('<!--INSERT ARTICLES BELOW-->\n');
-    except ValueError:
+        art_ln = lines.index('<!--INSERT ARTICLES BELOW-->\n')
+        mod_ln = lines.index('<!--LASTMOD DATE BELOW-->\n')
+    except ValueError: 
+        print "Failed to find line; terminating"
         sys.exit(1)
 
 href = raw_input("URL: ")
@@ -14,6 +16,7 @@ title = raw_input("Title: ")
 auth = raw_input("Author: ")
 
 with open('index.html', 'w') as f:
-    lines.insert(ln + 1, '<li><a href="%s" target="_blank">"%s"</a> by %s</li>\n' % (href, title, auth))
+    lines.insert(art_ln + 1, '<li><a href="%s" target="_blank">"%s"</a> by %s</li>\n' % (href, title, auth))
+    lines[mod_ln + 2] = 'Last updated %s.\n' % datetime.datetime.now().strftime('%d %B %Y')
     f.writelines(lines)
 
