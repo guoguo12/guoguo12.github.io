9333de400773e603d35f9ffbf5a2dbe70274c9f7
Allen Guo <guoguo12@gmail.com>
2015-11-11 17:37:44 -0800

    Hide old read articles; update article.py

--- article.py
+++ article.py
@@ -18,5 +18,12 @@ auth = raw_input("Author: ")
 with open('index.html', 'w') as f:
     lines.insert(art_ln + 1, '<li><a href="%s" target="_blank">"%s"</a> by %s</li>\n' % (href, title, auth))
     lines[mod_ln + 2] = 'Last updated %s.\n' % datetime.datetime.now().strftime('%d %B %Y')
+    
+    arcf_ln = lines.index('<!--ARCHIVE FROM ABOVE-->\n')
+    arct_ln = lines.index('<!--ARCHIVE TO BELOW-->\n')
+    to_archive = lines[arcf_ln - 1]
+    del lines[arcf_ln - 1]
+    lines.insert(arct_ln, to_archive)
+
     f.writelines(lines)
 
